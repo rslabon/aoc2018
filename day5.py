6e14b56c2b95d02d2cd57e23f10bd2a1b04cf387
rslabon <rslabon@gmail.com>
2025-07-14 19:06:23 +0200

    day 5 - done

--- day5.py
+++ day5.py
@@ -4,65 +4,18 @@ with open("./resources/day5.txt") as f:
     polymers = f.read().strip()
 
 
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-        self.previous = None
-
-    def append(self, other_node):
-        self.next = other_node
-        other_node.previous = self
-        return other_node
-
-    def remove(self):
-        next = self.next
-        previous = self.previous
-        if previous:
-            previous.next = next
-        if next:
-            next.previous = previous
-
-        self.next = None
-        self.previous = None
-
-    def __repr__(self):
-        return f"[{self.value}]"
-
-
 def reduce(polymers):
-    start = None
-    current = None
+    stack = []
     for p in polymers:
-        if not current:
-            current = Node(p)
-            start = current
+        if not stack:
+            stack.append(p)
+            continue
+        elif stack[-1].lower() == p.lower() and stack[-1] != p:
+            stack.pop()
         else:
-            current = current.append(Node(p))
+            stack.append(p)
 
-    left = start
-    right = left.next
-    deleted = 0
-    while left != right and left and right:
-        if left.value.lower() == right.value.lower() and left.value != right.value:
-            d1, d2 = left, right
-            if not left.previous:
-                left = right.next
-                right = left.next if left else None
-            elif not right.next:
-                right = left.previous
-                left = right.previous if right else None
-            else:
-                left = left.previous
-                right = right.next
-            d1.remove()
-            d2.remove()
-            deleted += 2
-        else:
-            left = left.next
-            right = right.next
-
-    return deleted
+    return len(stack)
 
 
 def remove(line, c):
@@ -70,14 +23,14 @@ def remove(line, c):
 
 
 def part1():
-    print(len(polymers) - reduce(polymers))
+    print(reduce(polymers))
 
 
 def part2():
     min_length = float("inf")
     for char in range(ord('a'), ord('z') + 1):
         new_polymer = remove(polymers, chr(char))
-        min_length = min(len(new_polymer) - reduce(new_polymer), min_length)
+        min_length = min(reduce(new_polymer), min_length)
     print(min_length)
 
 
