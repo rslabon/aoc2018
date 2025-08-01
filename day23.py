4a52708b7dd406003d3d441994ce4afe953d8b9d
Rafal Slabon <rafal.slabon@ocado.com>
2025-08-01 14:59:00 +0200

    day 23 - in progress

--- day23.py
+++ day23.py
@@ -1,3 +1,4 @@
+import functools
 import re
 
 lines = """
@@ -10,6 +11,15 @@ pos=<0,0,3>, r=1
 pos=<1,1,1>, r=1
 pos=<1,1,2>, r=1
 pos=<1,3,1>, r=1
+""".strip().splitlines()
+
+lines = """
+pos=<10,12,12>, r=2
+pos=<12,14,12>, r=2
+pos=<16,12,12>, r=4
+pos=<14,14,14>, r=6
+pos=<50,50,50>, r=200
+pos=<10,10,10>, r=5
 """.strip().splitlines()
 
 with open('./resources/day23.txt') as f:
@@ -29,6 +39,32 @@ def manhatan_distance(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1]) + abs(a[2] - b[2])
 
 
+def intersection(ranges):
+    ranges = sorted(ranges)
+    stack = []
+    for r in ranges:
+        if not stack:
+            stack.append(r)
+        elif r[0] > stack[-1][1]:
+            stack.append(r)
+        else:
+            if r[0] <= stack[-1][1]:
+                stack[-1][0] = r[0]
+            if r[1] <= stack[-1][1]:
+                stack[-1][1] = r[1]
+
+    return stack
+
+
+@functools.cache
+def number_in_range(pos):
+    in_range = 0
+    for nanobot in set(nanobots):
+        if manhatan_distance(pos, nanobot) <= nanobot[3]:
+            in_range += 1
+    return in_range
+
+
 def part1():
     largest_signal_radius = sorted(nanobots, key=lambda x: x[3], reverse=True)[0]
     in_range = 0
@@ -39,4 +75,47 @@ def part1():
     print(in_range)
 
 
-part1()
+def xxx(nanobots, x, y, z, r):
+    in_range = 0
+    for other_nanobot in nanobots:
+        distance = manhatan_distance(other_nanobot, (x, y, z))
+        if distance <= other_nanobot[3] + r:
+            in_range += 1
+    return in_range
+
+
+def part2(nanobots):
+    nanobots = set(nanobots)
+    ranges = dict()
+    for nanobot in nanobots:
+        in_range = 0
+        for other_nanobot in nanobots - {nanobot}:
+            distance = manhatan_distance(other_nanobot, nanobot)
+            if distance <= other_nanobot[3] + nanobot[3]:
+                in_range += 1
+        ranges[nanobot] = in_range
+
+    nano_in_ranges, _, (x, y, z, r) = list(sorted(map(lambda x: (-x[1], x[0][-1], x[0]), ranges.items())))[0]
+    nano_in_ranges = - nano_in_ranges
+    nanobots -= {(x, y, z, r)}
+    rr = r
+    while True:
+        k = xxx(nanobots, x, y, z, r)
+        if k < nano_in_ranges:
+            r += 1
+            break
+        r -= 1
+    print(rr, r)
+
+# print(xranges)
+# print(intersection(xranges))
+#
+# print(yranges)
+# print(intersection(yranges))
+#
+# print(zranges)
+# print(intersection(zranges))
+
+
+# part1()
+part2(nanobots)
