b623cff3e278f4615239c77005b11c2c842eb8e9
rslabon <rslabon@gmail.com>
2025-08-02 17:05:23 +0200

    day 21 - part1

--- day21.py
+++ day21.py
@@ -1,4 +1,161 @@
-from day16 import addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr
+import random
+
+DEBUG = True
+
+def addr(registers, A, B, C):
+    registers[C] = registers[A] + registers[B]
+    if DEBUG:
+        print(f"ADD into {C} = {registers[A]}[R={A}] + {registers[B]}[R={B}] = {registers[C]}")
+        if A == 0 or B == 0:
+            raise "XXXX"
+        if C == 5:
+            print(f"IP={registers[5]}")
+
+
+def addi(registers, A, B, C):
+    registers[C] = registers[A] + B
+    if DEBUG:
+        print(f"ADD into {C} = {registers[A]}[R={A}] + {B} = {registers[C]}")
+        if C == 5:
+            print(f"IP={registers[5]}")
+
+
+def mulr(registers, A, B, C):
+    registers[C] = registers[A] * registers[B]
+    if DEBUG:
+        print(f"MUL into {C} = {registers[A]}[R={A}] * {registers[B]}[R={B}] = {registers[C]}")
+        if A == 0 or B == 0:
+            raise "XXXX"
+        if C == 5:
+            print(f"IP={registers[5]}")
+
+
+def muli(registers, A, B, C):
+    registers[C] = registers[A] * B
+    if DEBUG:
+        print(f"MUL into {C} = {registers[A]}[R={A}] * {B} = {registers[C]}")
+        if A == 0:
+            raise "XXXX"
+        if C == 5:
+            print(f"IP={registers[5]}")
+
+
+def banr(registers, A, B, C):
+    registers[C] = registers[A] & registers[B]
+    if DEBUG:
+        print(f"AND into {C} = {registers[A]}[R={A}] & {registers[B]}[R={B}] = {registers[C]}")
+        if A == 0 or B == 0:
+            raise "XXXX"
+        if C == 5:
+            print(f"IP={registers[5]}")
+
+
+def bani(registers, A, B, C):
+    registers[C] = registers[A] & B
+    if DEBUG:
+        print(f"AND into {C} = {registers[A]}[R={A}] & {B} = {registers[C]}")
+        if A == 0:
+            raise "XXXX"
+        if C == 5:
+            print(f"IP={registers[5]}")
+
+
+def borr(registers, A, B, C):
+    registers[C] = registers[A] | registers[B]
+    if DEBUG:
+        print(f"OR into {C} = {registers[A]}[R={A}] | {registers[B]}[R={B}] = {registers[C]}")
+        if A == 0 or B == 0:
+            raise "XXXX"
+        if C == 5:
+            print(f"IP={registers[5]}")
+
+
+def bori(registers, A, B, C):
+    registers[C] = registers[A] | B
+    if DEBUG:
+        print(f"OR into {C} = {registers[A]}[R={A}] | {B} = {registers[C]}")
+        if A == 0:
+            raise "XXXX"
+        if C == 5:
+            print(f"IP={registers[5]}")
+
+
+def setr(registers, A, B, C):
+    registers[C] = registers[A]
+    if DEBUG:
+        print(f"SET into {C} = {registers[A]}[R={A}]")
+        if A == 0:
+            raise "XXXX"
+        if C == 5:
+            print(f"IP={registers[5]}")
+
+
+def seti(registers, A, B, C):
+    registers[C] = A
+    if DEBUG:
+        print(f"SET into {C} = {A}")
+        if C == 5:
+            print(f"IP={registers[5]}")
+
+
+def gtir(registers, A, B, C):
+    registers[C] = 1 if A > registers[B] else 0
+    if DEBUG:
+        print(f"{C} = if {A} > {registers[B]}[R={B}] 1 else 0 = {registers[C]}")
+        if B == 0:
+            raise "XXXX"
+        if C == 5:
+            print(f"IP={registers[5]}")
+
+
+def gtri(registers, A, B, C):
+    registers[C] = 1 if registers[A] > B else 0
+    if DEBUG:
+        print(f"{C} = if {registers[A]}[R={A}] > {B} 1 else 0 = {registers[C]}")
+        if A == 0:
+            raise "XXXX"
+        if C == 5:
+            print(f"IP={registers[5]}")
+
+
+def gtrr(registers, A, B, C):
+    registers[C] = 1 if registers[A] > registers[B] else 0
+    if DEBUG:
+        print(f"{C} = if {registers[A]}[R={A}] > {registers[B]}[R={B}] 1 else 0 = {registers[C]}")
+        if A == 0 or B == 0:
+            raise "XXXX"
+        if C == 5:
+            print(f"IP={registers[5]}")
+
+
+def eqir(registers, A, B, C):
+    registers[C] = 1 if A == registers[B] else 0
+    if DEBUG:
+        print(f"{C} = if {A} == {registers[B]}[R={B}] 1 else 0 = {registers[C]}")
+        if B == 0:
+            raise "XXXX"
+        if C == 5:
+            print(f"IP={registers[5]}")
+
+
+def eqri(registers, A, B, C):
+    registers[C] = 1 if registers[A] == B else 0
+    if DEBUG:
+        print(f"{C} = if {registers[A]}[R={A}] == {B} 1 else 0 = {registers[C]}")
+        if A == 0:
+            raise "XXXX"
+        if C == 5:
+            print(f"IP={registers[5]}")
+
+
+def eqrr(registers, A, B, C):
+    registers[C] = 1 if registers[A] == registers[B] else 0
+    if DEBUG:
+        print(f"{C} = if {registers[A]}[R={A}] == {registers[B]}[R={B}] 1 else 0 = {registers[C]}")
+        if A == 0 or B == 0:
+            raise "XXXX"
+        if C == 5:
+            print(f"IP={registers[5]}")
 
 
 def execute(instructions, registers):
@@ -14,6 +171,8 @@ def execute(instructions, registers):
         instruction = instructions[ip]
         op_name, *args = instruction.split(" ")
         before = registers[:]
+
+        print(instruction)
 
         args = map(int, args)
 
@@ -54,7 +213,7 @@ def execute(instructions, registers):
 
         ip = registers[bound] + 1
         total += 1
-        if total >= 100:
+        if total > 2000:
             raise RuntimeError("Too many iterations, something is wrong")
 
     return total
@@ -84,12 +243,17 @@ def xxx(start, end):
         print(mid)
         raise RuntimeError(mid)
 
-    xxx(start, mid - 1)
-    xxx(mid + 1, end)
+    random_boolean = random.choice([True, False])
+    if random_boolean:
+        xxx(start, mid)
+        xxx(mid + 1, end)
+    else:
+        xxx(mid + 1, end)
+        xxx(start, mid)
 
 
-for i in range(1000_000_000):
-    print("checking", i)
-    if check(i):
-        print(i)
-        break
+# check(0, True)
+# xxx(0, 5000_000_000)
+DEBUG=False
+print(check(30842))
+
